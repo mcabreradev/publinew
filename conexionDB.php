7a797c100e508dbaba777ef0dd104e5b5572fb62
Miguelangel Cabrera <mcabrera.dev@gmail.com>
2016-02-21 23:54:03 -0300

    se modifica la conexion a la base de datos

--- conexionDB.php
+++ conexionDB.php
@@ -1,30 +1,41 @@
 <?php
 
+$host = 'localhost';
+$user = 'root';
+$password = 'root';
+$database = 'publinew';
 
+$con = mysql_connect($host, $user, $password);
+if (!$con) {
+    die('No se conecto a la BD: '.mysql_error());
+}
 
-function conectar() {
-    $host = "localhost";
-    $user = "root";
-    $password = "noname";
-    $database = "publinew";
-    try{
+//esta funcion selecciona la base de datos a utilizar
+$bd = mysql_select_db($database);
+if (!$bd) {
+    die('No se encontro la BD seleccionada');
+}
+
+function conectar()
+{
+    try {
         $con = new mysqli($host, $user, $password, $database);
+
         return $con;
-    }  catch (Exception $e){
+    } catch (Exception $e) {
         echo $e->getMessage();
     }
 }
 
-function ejecutar($query) {
+function ejecutar($query)
+{
     $con = conectar();
     if ($con) {
         $result = $con->query($query);
         $con->close();
+
         return $result;
-    }else{
+    } else {
         echo 'Ha ocurrido un error en la base de datos';
     }
 }
-
-
-?>
